0d2ed56a757cfcf9376af6f87f1446ecc6c430bc
AlexSmirnov9107 <shadowalex9107@gmail.com>
2017-11-21 13:42:38 +0600

    done

--- Practice/Prartice1/Prartice1/Program.cs
+++ Practice/Prartice1/Prartice1/Program.cs
@@ -10,15 +10,83 @@ namespace Prartice1
     {
         static void Main(string[] args)
         {
-            ToDo1();
-            ToDo2();
-            ToDo3();
-            ToDo4();
-            ToDO5();
+            Console.Write("Введите номер задания (1-8) ->");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ToDo1();
+                    break;
+                case "2":
+                    ToDo2();
+                    break;
+                case "3":
+                    ToDo3();
+                    break;
+                case "4":
+                    ToDo4();
+                    break;
+                case "5":
+                    ToDo5();
+                    break;
+                case "6":
+                    ToDo6();
+                    break;
+                case "7":
+                    ToDo7();
+                    break;
+                case "8":
+                    ToDo8();
+                    break;
+
+            }
+
+           
             Console.ReadKey();
         }
 
-        private static void ToDO5()
+        private static void ToDo8()
+        {
+            Console.WriteLine("8.	Вычислить значение логического выражения при всех возможных значениях логических величин X и Y: \n a.	не X и не Y; \n b.	X или (не X и Y); \n c.	(не X и Y) или Y. \n \n");
+            bool x = false;
+            bool y = true;
+            Console.Write("X = {0:G}\nY = {1:G}\n", x, y);
+
+            Console.Write("не X и не Y = {0:G}\n", !x && !y);
+            Console.Write("X или (не X и Y) = {0:G}\n", !x || (!x && !y));
+            Console.Write("(не X и Y) или Y = {0:G}\n", (!x && y) || y);
+
+
+
+        }
+
+        private static void ToDo7()
+        {
+            Console.WriteLine("7.	В трехзначном числе x зачеркнули его вторую цифру. Когда к образованному при этом двузначному числу справа приписали вторую цифру числа x, то получилось число 456. Найти число x");
+            Console.Write("Введите трехзначное число ->");
+            int chislo = int.Parse(Console.ReadLine());
+            int first = chislo / 100;
+            int second = (chislo % 100) / 10;
+            int third = chislo % 10;
+            Console.Write("x = {0:G}{1:G}{2:G}", first, third, second);
+        }
+
+        private static void ToDo6()
+        {
+            Console.WriteLine("6.	Дано четырехзначное число. Найти: \na.сумму его цифр;\nb.произведение его цифр.");
+            Console.Write("Введите четырехзначное число ->");
+            int chislo = int.Parse(Console.ReadLine());
+            int first = chislo / 1000;
+            int second = (chislo % 1000) / 100;
+            int third = ((chislo % 1000) % 100) / 10;
+            int fourth = chislo % 10;
+            int sum = first + second + third + fourth;
+            int multi = first * second * third * fourth;
+            Console.Write("а. Сумма = {0:G}\n", sum);
+            Console.Write("а. Произведение = {0:G}\n", multi);
+
+        }
+
+        private static void ToDo5()
         {
             Console.WriteLine("5.	Дано двузначное число. Найти:\n" +
                 "a.число десятков в нем;\n" +
@@ -28,10 +96,17 @@ namespace Prartice1
             Console.Write("Введите двухзначное число ->");
             int chislo = int.Parse(Console.ReadLine());
             int firstChislo = chislo / 10;
-            int secondChislo = chislo - firstChislo*10;
-            count1 = (chislo - desyat * 10) == 1 ? count1++ : count1;
+            int secondChislo = chislo - firstChislo * 10;
+            int count1 = firstChislo == 1 ? 1 : 0;
+            count1 = secondChislo == 1 ? count1++ : count1;
+            int sum = firstChislo + secondChislo;
+            int multiply = firstChislo * secondChislo;
+            Console.Write("a. Десятков = {0:G}\n", firstChislo);
+            Console.Write("b. Число единиц = {0:G}\n", count1);
+            Console.Write("c. Сумма цифр = {0:G}\n", sum);
+            Console.Write("d. Произведение = {0:G}\n", multiply);
 
-            
+
         }
 
         private static void ToDo4()
