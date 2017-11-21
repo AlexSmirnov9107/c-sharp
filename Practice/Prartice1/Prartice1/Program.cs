8f8fe3baca402bea7a040673c5aa8b73b9ee9415
AlexSmirnov9107 <shadowalex9107@gmail.com>
2017-11-21 11:15:07 +0600

    Todo 1

--- Practice/Prartice1/Prartice1/Program.cs
+++ Practice/Prartice1/Prartice1/Program.cs
@@ -10,6 +10,24 @@ namespace Prartice1
     {
         static void Main(string[] args)
         {
+            ToDo1();
+            ToDo2();
+            Console.ReadKey();
+        }
+
+        private static void ToDo2()
+        {
+            Console.WriteLine("2.Дан радиус окружности. Найти длину окружности и площадь круга");
+        }
+
+        private static void ToDo1()
+        {
+            Console.WriteLine("вычисления значения функции y = 7x ^ 2 - 3x + 4  при любом значении x;");
+            Console.Write("Введите x ->");
+            int x = int.Parse(Console.ReadLine());
+            int y = 7 * x * x  - 3 * x + 4;
+            Console.Write("y = {0:G} ", y);
+
         }
     }
 }
