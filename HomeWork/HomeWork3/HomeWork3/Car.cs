91b619317082da85e2dd990d9e818de31a5231b5
AlexSmirnov9107 <shadowalex9107@gmail.com>
2017-11-27 09:05:18 +0600

    Done HomeWork 3

---
+++ HomeWork/HomeWork3/HomeWork3/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    partial class Car
+    {
+        static public int countCars;
+        static public Car oldestCar;
+        private string vendor;
+        private string mark;
+        private int year;
+        private double capacity;
+        private string color;
+
+        static Car()
+        {
+            countCars = 0 ;
+            oldestCar = null;
+        }
+        public Car()
+        {
+           year= 1991;
+           countCars++;
+        }
+        public Car(string vendor,string mark, int year, double capacity, string color)
+        {
+            countCars++;
+            this.vendor = vendor;
+            this.mark = mark;
+            this.year = year;
+            this.capacity = capacity;
+            this.color = color;
+
+            if (oldestCar != null)
+            {
+                if (oldestCar.GetYear() > year)
+                {
+                    oldestCar = this;
+                }
+
+            }
+            else
+            {
+                oldestCar = this;
+            }
+        }
+        public string GetColor()
+        {
+            return color;
+        }
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+        public int GetYear()
+        {
+            return year;
+        }
+        public void SetYear(int year)
+        {
+          
+            this.year = year;
+        }
+        public void GetCountCars(out int count)
+        {
+            count = countCars;
+        }
+    }
+}
---
+++ HomeWork/HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car[] cars = new Car[5];
+            cars[0] = new Car("MB", "GLK", 2015, 2.0, "Brown");
+            cars[1] = new Car("MB", "GLK", 2016, 2.0, "Brown");
+            cars[2] = new Car("MB", "GLK", 2014, 2.0, "Brown");
+            cars[3] = new Car("MB", "GLK", 2013, 2.0, "Brown");
+            cars[4] = new Car("MB", "GLK", 2017, 2.0, "Brown");
+            Console.WriteLine(Car.oldestCar.GetYear());
+            Console.ReadKey();
+        }
+    }
+}
