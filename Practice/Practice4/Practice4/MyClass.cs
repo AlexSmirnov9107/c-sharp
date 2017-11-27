c0c104ebe7aa9fc4b9564fc46bd2ab2c9cd7077a
AlexSmirnov9107 <shadowalex9107@gmail.com>
2017-11-27 10:52:50 +0600

    Practice 4

---
+++ Practice/Practice4/Practice4/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Practice4
+{
+    public class MyClass
+    {
+        public string SayMyName(string name)
+        {
+            if (name == "")
+            {
+                throw new Exception("I dont know your name");
+            }
+            return name;
+
+        }
+
+    }
+}
---
+++ Practice/Practice4/Practice4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass cls = new MyClass();
+            Console.WriteLine(cls.SayMyName("Alex"));
+            try
+            {
+                cls.SayMyName("");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
