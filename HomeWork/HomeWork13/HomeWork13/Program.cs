90aac01a960952ad5d07a86ac3d8766b01e9a661
AlexSmirnov9107 <shadowalex9107@gmail.com>
2017-12-11 10:56:06 +0600

    fixed

--- HomeWork/HomeWork13/HomeWork13/Program.cs
+++ HomeWork/HomeWork13/HomeWork13/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Practice13
+namespace HomeWork13
 {
     class Program
     {
@@ -13,20 +13,17 @@ namespace Practice13
             ArrayList array = new ArrayList() {
                 1,2,3,4,5,6
             };
-            ArrayList tmp = new ArrayList();
             int sum = 0;
             for (int i = 0; i < array.Count; i++)
             {
                 sum += (int)array[i];
-                if ((int)array[i] % 2 == 0) // 
-                    tmp.Add(array[i]); // четные
+                if ((int)array[i] % 2 == 1) // 
+                    array.RemoveAt(i); // Удаляем Нечетные
             }
-            array.Clear();
-            array.SetRange(0, tmp);
             foreach (var item in array)
             {
-                if ((int)item > sum/array.Count) // Элементы больше среднеарифметического
-                Console.WriteLine(item);
+                if ((int)item > sum / array.Count) // Элементы больше среднеарифметического
+                    Console.WriteLine(item);
             }
             Console.ReadLine();
         }
