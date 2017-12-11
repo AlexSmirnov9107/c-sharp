5cc5879664df60af7b05f2b8259dc46a33d4cf92
AlexSmirnov9107 <shadowalex9107@gmail.com>
2017-12-11 10:45:08 +0600

    practive 13

---
+++ Practice/Practice13/Practice13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList() {
+                1,2,3,4,5,6
+            };
+            int sum = 0;
+            for (int i = 0; i < array.Count; i++)
+            {
+                sum += (int)array[i];
+                if ((int)array[i] % 2 == 1) // 
+                    array.RemoveAt(i); // Удаляем Нечетные
+            }
+            foreach (var item in array)
+            {
+                if ((int)item > sum/array.Count) // Элементы больше среднеарифметического
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
