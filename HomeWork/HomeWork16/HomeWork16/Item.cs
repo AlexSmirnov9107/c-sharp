91a52944047f985ab26f90fc720af9d50150e268
AlexSmirnov9107 <shadowalex9107@gmail.com>
2017-12-22 11:48:13 +0600

    homework16

---
+++ HomeWork/HomeWork16/HomeWork16/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork16
+{
+    public class Item
+    {
+        //Title, Link, Description, PubDate.
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string PubDate { get; set; }
+        public string Guid { get; set; }
+
+
+    }
+}
---
+++ HomeWork/HomeWork16/HomeWork16/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace HomeWork16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> news = new List<Item>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load("habrahabr.ru.xml");
+            var root = doc.DocumentElement.GetElementsByTagName("item");
+            foreach (XmlElement item in root )
+            {
+                
+                news.Add(new Item {
+                    
+                    Title = item.GetElementsByTagName("title")[0].InnerText,
+                    Description = item.GetElementsByTagName("description")[0].InnerText,
+                    Link = item.GetElementsByTagName("link")[0].InnerText,
+                    Guid = item.GetElementsByTagName("guid")[0].InnerText,
+                    PubDate = item.GetElementsByTagName("pubDate")[0].InnerText
+
+                });
+            }
+
+            XmlDocument docStudents = new XmlDocument();
+            XmlElement students = docStudents.CreateElement("students");
+            XmlElement student = docStudents.CreateElement("student");
+            student.SetAttribute("name", "Alex");
+            student.SetAttribute("LastName", "Sm");
+            students.AppendChild(student);
+
+            student = docStudents.CreateElement("student");
+            student.SetAttribute("name", "mikhail");
+            student.SetAttribute("LastName", "sm");
+            students.AppendChild(student);
+
+            docStudents.AppendChild(students);
+            docStudents.Save("Students.xml");
+
+        }
+    }
+}
