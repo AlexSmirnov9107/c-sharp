dbad96ce0038b228823fd048ef59ccf3b2d65c69
AlexSmirnov9107 <shadowalex9107@gmail.com>
2017-12-22 11:09:47 +0600

    homework 15

---
+++ HomeWork/HomeWork15/HomeWork15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader stream = new StreamReader("fibo.txt"))
+            {
+                string inputStr = stream.ReadLine();
+                var numbers = inputStr.Split(' ').ToList();
+                int count = numbers.Count;
+                for (int i = 0; i < count ; i++)
+                {
+                    int number = int.Parse(numbers[numbers.Count - 2]) + int.Parse(numbers[numbers.Count - 1]);
+                    numbers.Add(number.ToString());
+                }
+                int sum = 0;
+                foreach (var item in numbers)
+                {
+                    sum += int.Parse(item);
+                    Console.Write(item + " ");
+                }
+
+                using (StreamWriter streamWriter = new StreamWriter("OUTPUT.txt"))
+                {
+                    
+                    streamWriter.WriteLine(sum);    
+                                                        
+                }
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
