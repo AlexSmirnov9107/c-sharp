95c2bc7a52bf7fee344cca16738a55c97c341e5f
AlexSmirnov9107 <shadowalex9107@gmail.com>
2017-12-27 10:16:41 +0600

    homework18

---
+++ HomeWork/HomeWork18/HomeWork18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            1.С помощью рефлексии получить список методов класса Console и вывести на экран.
+            //2.Описать класс с несколькими свойствами. Создать экземпляр класса и инициализировать его свойства.С помощью рефлексии получить свойства и их значения из созданного экземпляра класса. Вывести полученные значения на экран
+            Type type = Type.GetType("System.Console");
+            foreach (var method in type.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+            }
+            Console.WriteLine("-----------------------------------");
+
+
+            SomeClass someclass = new SomeClass(15, "Вуася");
+            Type type2 = someclass.GetType();
+            foreach (var prop in type2.GetProperties())
+            {
+                Console.WriteLine(prop.Name + " = " +prop.GetValue(someclass));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork/HomeWork18/HomeWork18/SomeClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork18
+{
+    public class SomeClass
+    {
+        public int FirstProperty { get; set; }
+        public string SecondProperty { get; set; }
+        public SomeClass(int first,string second)
+        {
+            FirstProperty = first;
+            SecondProperty = second;
+        }
+    }
+}
