ddedd12d0e57a2c28818caad761a51c25e3d6547
AlexSmirnov9107 <shadowalex9107@gmail.com>
2018-01-29 11:10:20 +0600

    hm 9

---
+++ WPF/WpfApp9/WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opendialog = new OpenFileDialog();
+            opendialog.Filter = "Image Files (JPG,PNG,GIF)|*.JPG;*.PNG;*.GIF";
+            opendialog.ShowDialog();
+            if (opendialog.FileName != "")
+            {
+              
+                BitmapImage image = new BitmapImage(new Uri(opendialog.FileName));
+               
+                ImageBrush imgbrush = new ImageBrush();
+                imgbrush.ImageSource =image;
+                canvas.Background = imgbrush;
+                imageName.Text = opendialog.FileName;
+            }
+        }
+    }
+}
